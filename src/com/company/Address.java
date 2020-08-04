54495ac2df33cede6aba53fd5731e3861acaa28a
olchik1987 <olga1987pt@gmail.com>
2020-08-03 21:10:49 -0700

    first commit

---
+++ src/com/company/Address.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Address {
+    int houseNumber;
+    String streetName;
+    String city;
+    String state;
+    int zipCode;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Хранить контакты
+        //Поиск контактов
+        //Поиск по фамилии
+        //Ввод фамилии
+        //Вывод на экран
+    }
+}
